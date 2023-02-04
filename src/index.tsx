72ef00aaff299d86b1a178d20b5158722159b79e
b0gdanwork <250101zx>
2023-02-04 18:55:06 +0500

    modern webpack

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import s from './index.module.scss'
+
+export default function App() {
+  return (
+    <div className={s.all}>indexindex</div>
+  )
+}
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(<App/>);
