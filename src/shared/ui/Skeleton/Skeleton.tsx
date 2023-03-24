a379a1bc49ff61d941e1839de31d68a225deeb84
bogdan <b0gdanwork@ya.ru>
2023-03-24 13:17:50 +0500

    add skeleton

---
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -0,0 +1,30 @@
+
+import { memo } from 'react'
+import s from './Skeleton.module.scss'
+
+interface Props {
+  width: number | string,
+  height: number | string,
+  border: number
+}
+
+function Skeleton (props: Props) {
+
+  const {
+    width,
+    height,
+    border   
+  } = props
+
+  const style = {
+    width,
+    height,
+    borderRadius: `${border}px`
+  }
+
+  return (
+    <div style={style} className={s.skeleton}/>
+  )
+}
+
+export default memo(Skeleton)
